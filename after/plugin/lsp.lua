075dabe9bb5c35e245925b0e2ddd1d123bd49c69
Alex Labonne <alex.labonne@iugmd69wq6vr7c.fritz.box>
2024-01-15 15:08:45 +0100

    update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,47 +2,50 @@ local lsp = require("lsp-zero")
 require("neodev").setup({})
 require("neoconf").setup()
 require("mason").setup()
-require('mason-lspconfig').setup({
-    ensure_installed = { "volar", "tsserver" }
+require("mason-lspconfig").setup({
+	ensure_installed = { "volar", "tsserver" },
 })
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
-require('mason-lspconfig').setup_handlers({
-    function(server_name)
-        local server_config = {}
-        if require("neoconf").get(server_name .. ".disable") then
-            server_config = {
-                autostart = false,
-                root_dir = function()
-                    return false
-                end,
-                single_file_support = false,
-            }
-            -- return
-        end
-        if server_name == "volar" then
-            server_config.filetypes = { 'vue', 'typescript', 'javascript' }
-        end
-        lspconfig[server_name].setup(server_config)
-    end,
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		local server_config = {}
+		if require("neoconf").get(server_name .. ".disable") then
+			server_config = {
+				autostart = false,
+				root_dir = function()
+					return false
+				end,
+				single_file_support = false,
+			}
+			-- return
+		end
+		if server_name == "volar" then
+			server_config.filetypes = { "vue", "typescript", "javascript" }
+		end
+		lspconfig[server_name].setup(server_config)
+	end,
+	-- lua_ls = function()
+	--     local lua_opts = lsp_zero.nvim_lua_ls()
+	--     require('lspconfig').lua_ls.setup(lua_opts)
+	-- end,
+	--
 })
-
 
 lsp.preset({
-    name = "minimal",
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = true,
+	name = "minimal",
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = true,
 })
 
 -- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
+-- lsp.nvim_workspace()
 
 -- lsp.ensure_installed({
 --     "volar",
 --     "tsserver",
 -- })
-
 
 -- Fix Undefined global 'vim'
 -- lsp.configure("luaformatter", {
@@ -57,10 +60,10 @@ lsp.nvim_workspace()
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-c>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<CR>"] = cmp.mapping.confirm({ select = true }),
+	["<C-c>"] = cmp.mapping.complete(),
 })
 
 -- disable completion with tab
@@ -68,33 +71,37 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 cmp_mappings["<Tab>"] = nil
 cmp_mappings["<S-Tab>"] = nil
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings,
+cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp_mappings,
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = true,
-    sign_icons = {
-        error = "E",
-        warn = "W",
-        hint = "H",
-        info = "I",
-    },
+	suggest_lsp_servers = true,
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+	-- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
 -- require("typescript").setup({
@@ -122,5 +129,5 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true,
+	virtual_text = true,
 })
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,43 +10,43 @@ vim.keymap.set("n", "<leader>vs", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
 vim.keymap.set("n", "<leader>fw", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set("n", "<leader>fs", function()
-	builtin.live_grep({ search = vim.fn.input("Grep Word> ") })
+    builtin.live_grep({ search = vim.fn.input("Grep Word> ") })
 end)
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 telescope.setup({
-	defaults = {
-		-- pickers      = {
-		--     find_files = {
-		--         layout_config = {
-		--             width = 0.95,
-		--         },
-		--     },
-		--     live_grep  = {
-		--         layout_config = {
-		--             width = 0.95,
-		--         },
-		--     },
-		-- },
+    defaults = {
+        -- pickers      = {
+        --     find_files = {
+        --         layout_config = {
+        --             width = 0.95,
+        --         },
+        --     },
+        --     live_grep  = {
+        --         layout_config = {
+        --             width = 0.95,
+        --         },
+        --     },
+        -- },
 
-		layout_config = {
-			preview_width = 0.75,
-			horizontal = { height = 0.9, width = 0.9 },
-		},
-		path_display = { truncate = 3 },
-		preview = {
-			hide_on_startup = true,
-		},
-		mappings = {
-			n = {
-				["<M-p>"] = action_layout.toggle_preview,
-			},
-			i = {
-				["<C-u>"] = false,
-				["<M-p>"] = action_layout.toggle_preview,
-			},
-		},
-	},
+        layout_config = {
+            preview_width = 0.75,
+            horizontal = { height = 0.9, width = 0.9 },
+        },
+        path_display = { truncate = 3 },
+        preview = {
+            hide_on_startup = true,
+        },
+        mappings = {
+            n = {
+                ["<M-p>"] = action_layout.toggle_preview,
+            },
+            i = {
+                ["<C-u>"] = false,
+                ["<M-p>"] = action_layout.toggle_preview,
+            },
+        },
+    },
 })
